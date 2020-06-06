bb2421691939066f7f8dc07e9bb214920b60bff0
kiran <kirann471@gmail.com>
2020-06-06 18:52:42 +0530

    Added custome class,movies class and displayed hard coded movies

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,7 +14,43 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name= "Customer 2" }
+            };
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            return Content("Id = " + Id);
+        }
+
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id=1, Name = "Shrek" },
+                new Movie { Id=2,Name="Wall-E"}
+            };
+        }
+        [Route("movies/released/{year:regex(\\d{4})}/{month}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
