69775312ea84b749af604a9708efb3b7d7ce71da
kiran <kirann471@gmail.com>
2020-06-08 19:40:13 +0530

    Upto Section 4 with Exercise

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -38,9 +38,48 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if(movie.Id==0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.NumberInStocks = movie.NumberInStocks;
+                movieInDb.Genre = movie.Genre;
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Movies");
+        }
+        public ActionResult New()
+        {
+            var genre = _context.Genres.ToList();
+            var viewModel = new MoviesFormViewModel
+            {
+                Genres = genre
+            };
+            return View("MovieForm",viewModel);
+        }
         public ActionResult Edit(int Id)
         {
-            return Content("Id = " + Id);
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == Id);
+            if(movie==null)
+            {
+                return HttpNotFound();
+            }
+            var viewModel = new MoviesFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+            return View("MovieForm", viewModel);
         }
 
         public ActionResult Index()
